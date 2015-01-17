cc0c6110340b08c74ad8c7dbb6e5e49dc2d3e8e9
Maxim Polyakov <mmonk@cpan.org>
2015-01-17 17:56:12 +0300

    Some refactoring on tests

--- t/lib/TCPClient.pm
+++ t/lib/TCPClient.pm
@@ -4,6 +4,7 @@ package Extended::Mock::Server;
 use Mouse;
 use Carp;
 use AnyEvent;
+use Test::More;
 #use AnyEvent::Socket;
 #use AnyEvent::Handle;
 
@@ -12,18 +13,27 @@ has timeout => (is=> 'rw', default => 1);
 has timer   => (is=> 'rw',); 
 has server  => (is=> 'rw',); 
 
+has connection  => (is =>'rw', required => 1);
+
+
+
 sub BUILD {
 	my $self = shift;
 
 	$self->timer( AE::timer $self->timeout,0, sub { undef $self->timer; fail( "tcp mock server exited for timeout") } );
 
-	$self->server (AnyEvent::MockTCPServer->new(connections =>
-		[ # Expected connections sequence
-			[ # first connection
-				[ code => sub { 'hey' }, 'received connect' ],
-			],
+	my $connections;
+	if ( ref $self->connection eq 'ARRAY' ) {
+		$connections = [ # Expected connections sequence
+			#[ # first connection
+				$self->connection,
+			#],
 		],
-	));
+	} else {
+		die "Fix this logic first!"
+	}
+
+	$self->server (AnyEvent::MockTCPServer->new(connections => $connections));
 
 }
 
@@ -46,10 +56,14 @@ use AnyEvent::MockTCPServer qw/:all/;
 use Data::Dumper;
 use Carp;
 
+has request_string => ( is=> 'rw', default=> 'HELLO');
+has reply_string   => ( is=> 'rw', default=> 'BYE' );
 
 sub test_connection {
 	my $self     = shift;
-	my $mockserv = Extended::Mock::Server->new();
+	my $mockserv = Extended::Mock::Server->new(connection => [
+		[ code => sub { 'hey' }, 'received connect' ],
+	]);
 
 	my ($host, $port) = $mockserv->server->connect_address;
 	my $client = App::ProxyMate::TCPClient->new( host=>$host, port=>$port);
@@ -68,72 +82,51 @@ sub test_connection {
 
 
 sub test_send_data_to_server {
+	my $self     = shift;
 
-	my ($request_string, $reply_string) = ('HELLO', 'BYE' );
-
-	my $server = AnyEvent::MockTCPServer->new(connections =>
-		[ # Expected connections sequence
-			[ # first connection
-				[ recv => $request_string, 'wait for "HELLO"' ],
-				[ send => $reply_string, 'send "BYE"' ],
-			],
-		],
-	);
-
-	my ($host,$port) = $server->connect_address;
+	my $mockserv = Extended::Mock::Server->new(connection => [
+		[ recv => $self->request_string, 'wait for "HELLO"' ],
+		[ send => $self->reply_string, 'send "BYE"' ],
+	]);
+	my ($host, $port) = $mockserv->server->connect_address;
 	my $client = App::ProxyMate::TCPClient->new( host=>$host, port=>$port);
 
-	my $send_hello_on_connect; $send_hello_on_connect = sub {
+	$client->connect( sub {
 		my $cl = shift;
 		fail 'client object should be passed into callback on successful connection' unless ref $cl eq ref $client;
-		$cl->send($request_string);
-	};
+		$cl->send($self->request_string);
+	});
 
-	$client->connect( $send_hello_on_connect );
-	$server->finished_cv->recv;
+	$mockserv->server->finished_cv->recv;
 
 }
 
 sub test_receive_data_from_server {
+	my $self     = shift;
 
-	my ($request_string, $reply_string) = ('HELLO', 'BYE' );
-
-	my $server = AnyEvent::MockTCPServer->new(connections =>
-		[ # Expected connections sequence
-			[ # first connection
-				[ send => $reply_string, 'send "BYE"' ],
-			],
-		],
-	);
-
-	my ($host,$port) = $server->connect_address;
+	my $mockserv = Extended::Mock::Server->new(connection => [
+		[ send => $self->reply_string, 'send "BYE"' ],
+	]);
+	my ($host, $port) = $mockserv->server->connect_address;
 	my $client = App::ProxyMate::TCPClient->new( host=>$host, port=>$port);
+
 	my $read_cv = AE::cv;
-	$client->on_read( sub {
-			my $data = shift;
-			$read_cv->send($data);
-		}
-	);
+	$client->on_read( sub { $read_cv->send(@_); });
 
 	$client->connect( sub { BAIL_OUT("Connection failed, which is impossible!!!") unless $_[0]; } );
-	$server->finished_cv->recv;
-	my $received_data = $read_cv->recv;
-	is($received_data, $reply_string, 'Received data from server via on_read callback');
+	$mockserv->server->finished_cv->recv;
+	my ($received_data) = $read_cv->recv;
+	is($received_data, $self->reply_string, 'Received data from server via on_read callback');
 
 }
 
 sub test_basic_error_handling {
-	my ($request_string, $reply_string) = ('HELLO', 'BYE' );
+	my $self     = shift;
 
-	my $server = AnyEvent::MockTCPServer->new(connections =>
-		[ # Expected connections sequence
-			[ # first connection
-				[ recv => $request_string, 'wait for "HELLO"' ],
-			],
-		],
-	);
-
-	my ($host,$port) = $server->connect_address;
+	my $mockserv = Extended::Mock::Server->new(connection => [
+		[ recv => $self->request_string, 'wait for "HELLO"' ],
+	]);
+	my ($host, $port) = $mockserv->server->connect_address;
 	my $client = App::ProxyMate::TCPClient->new( host=>$host, port=>$port);
 
 	my $cv = AE::cv;
@@ -142,16 +135,15 @@ sub test_basic_error_handling {
 	$client->connect( sub {
 		my $cl = shift;
 		fail 'client object should be passed into callback on successful connection' unless ref $cl eq ref $client;
-		$cl->send($request_string);
+		$cl->send($self->request_string);
 		}
 	);
-	$server->finished_cv->recv; # server closed connection at this point
+	$mockserv->server->finished_cv->recv; # server closed connection at this point
 	$client->send('TRY TALK TO GONE CLIENT');
 
 	my ( $msg )	= $cv->recv;
 	ok($msg && length($msg), 'error message passed into client_gone callback') ;
 	ok(!ref $msg, 'scalar message passed into client_gone callback') ;
-	#diag "client gone error msg: $msg";
 
 }
 
