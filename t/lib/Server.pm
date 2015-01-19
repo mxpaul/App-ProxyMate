82dd2c4fe938d61f740ae75fb0c5a4c2391b3412
Maxim <pms@nm.ru>
2015-01-19 23:45:02 +0300

    Add listen() and accept() into Server class
    Stolen tcp_server from Marc Lehmann's AnyEvent::Socket.
    Splited listen and accept into different subs, to be able accept
    in multiple children processes

--- t/lib/Server.pm
+++ t/lib/Server.pm
@@ -4,13 +4,35 @@ use App::ProxyMate::Server;
 use Data::Dumper;
 use Carp;
 use AnyEvent;
+use AnyEvent::Socket;
 
-use Helper qw(cvt); # This is really imported as AE::cvt
+use Helper qw(cvt); # This is really defined as AE::cvt, Helper not exporting anything
 
+sub test_bind_listen_accept {
+	my $self = shift;
 
-sub test_new_class {
+	my ($host,$port) = Helper::free_host_port;
 
-	ok 1, "Success in fail";
+	my $server = App::ProxyMate::Server->new(host=>$host, port=>$port);
+
+	eval { $server->listen }; ok(!$@, "bind succeed with no exceptions: $@");
+
+	eval { tcp_server $host, $port, sub {}; }; ok ($@, "Can't bind to same host port");
+
+	my $server_ready = AE::cvt 1;
+	$server->on_client_connection($server_ready);
+	$server->accept;
+
+	my $client_ready = AE::cvt 1;
+	tcp_connect $host,$port, $client_ready;
+
+	
+	my $server_fh = $server_ready->recv;
+	ok ($server_fh, "Server received connect $host:$port");
+
+	my $client_fh = $client_ready->recv;
+	ok ($client_fh, "Client able to connect to $host:$port");
+
 }
 
 1;
