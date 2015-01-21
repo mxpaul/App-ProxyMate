4846ffe78d33549b7a7202fceefc04734b0d057c
Maxim <pms@nm.ru>
2015-01-22 00:28:38 +0300

    Another test - an illustration of tcp_server usage

--- t/lib/Server.pm
+++ t/lib/Server.pm
@@ -5,6 +5,7 @@ use Data::Dumper;
 use Carp;
 use AnyEvent;
 use AnyEvent::Socket;
+use Socket qw(inet_ntoa);
 
 use Helper qw(cvt); # This is really defined as AE::cvt, Helper not exporting anything
 
@@ -32,6 +33,38 @@ sub test_bind_listen_accept {
 
 	my $client_fh = $client_ready->recv;
 	ok ($client_fh, "Client able to connect to $host:$port");
+
+}
+
+sub test_server_talk_to_client {
+	my ($host,$port) = Helper::free_host_port;
+
+	my ($srv_client_host, $srv_client_port);
+
+	my $server = App::ProxyMate::Server->new(host=>$host, port=>$port);
+	$server->on_client_connection( sub {
+			my $fh = shift or fail 'Server receives valid file handle on client connect';
+			(my $netint_host, $srv_client_port) = @_;
+			my $client_ip = inet_ntoa($netint_host);
+			my $bytes = syswrite($fh,'HELLO', 5,0);
+			is ($bytes,5, 'Server able talk to client');
+		}
+	);
+	$server->listen; $server->accept;
+
+	my $cv = AE::cvt 1;
+	tcp_connect $host,$port, sub {
+		my $fh = shift or fail ('Client handle not valid');	
+		
+		my $rw; $rw = AE::io $fh, 0, sub {
+			my $bytes = sysread($fh, my $data, 16);
+			undef $rw;
+			$cv->send($data);
+		};
+	};
+	my ($data) = $cv->recv;
+	is($data, 'HELLO', 'Client received message');
+
 
 }
 
